7c4c2308b597d4bb118f2ffad9d7bd46dab31de6
Kyoengpil Lee <kplee@cern.ch>
2017-03-21 11:31:41 +0900

    make cu box

--- source/src/WGR16DetectorConstruction_old.cc
+++ source/src/WGR16DetectorConstruction_old.cc
@@ -69,6 +69,7 @@
 
 using namespace std;
 
+
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 G4ThreadLocal WGR16MagneticField* WGR16DetectorConstruction::fMagneticField = 0;
@@ -77,114 +78,92 @@ G4ThreadLocal G4FieldManager* WGR16DetectorConstruction::fFieldMgr = 0;
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 WGR16DetectorConstruction::WGR16DetectorConstruction()
-: G4VUserDetectorConstruction(), fMessenger(0), fVisAttributes(),fScoringVolume(0)
+: G4VUserDetectorConstruction(), 
+  fMessenger(0),
+  fVisAttributes(),fScoringVolume(0)
 {
-	DefineCommands();
+    // define commands for this class
+    DefineCommands();
 }
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 WGR16DetectorConstruction::~WGR16DetectorConstruction()
 {
-	delete fMessenger;
-	
-	for (G4int i=0; i<G4int(fVisAttributes.size()); ++i)
-	{
-		delete fVisAttributes[i];
-	}
+    delete fMessenger;
+    
+    for (G4int i=0; i<G4int(fVisAttributes.size()); ++i) 
+    {
+      delete fVisAttributes[i];
+    }  
 }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 G4VPhysicalVolume* WGR16DetectorConstruction::Construct()
 {
-	G4bool checkOverlaps = false;
 
-	// -- meterials -- //
-	ConstructMaterials();
-	G4Material* vac = G4Material::GetMaterial("G4_Galactic");
-	G4Material* cu  = G4Material::GetMaterial("G4_Cu");
+    G4bool checkOverlaps = false;
 
-	const G4double cu_radlen = cu->GetRadlen(); // -- radiation length -- //
+    ConstructMaterials();
+    G4Material* vac = G4Material::GetMaterial("G4_Galactic");
 
-	// -- world -- //
-	G4VSolid* worldSolid 
-	= new G4Box("worldBox",10.*m,10.*m,10.*m);
-	G4LogicalVolume* worldLogical
-	= new G4LogicalVolume(worldSolid,vac,"worldLogical");
-	G4VPhysicalVolume* worldPhysical
-	= new G4PVPlacement(0,G4ThreeVector(),worldLogical,"worldPhysical",0, false,0,checkOverlaps);
+    G4Material* vac = G4Material::GetMaterial("G4_Galactic");
+    G4Material* cu  = G4Material::GetMaterial("G4_Cu");
 
-	G4double pi = 3.14159265359;
-	G4double radius = 1.8; // -- unit: m -- //
-	G4double Circumference = 2 * pi * radius;
-	G4double nTower_PhiDir = 283.0;
-	G4double CuLen_PhiDir = nTower_PhiDir / Circumference;
+    const G4double cu_radlen = cu->GetRadlen();
 
-	G4double CuLen_EtaDir = CuLen_PhiDir;
-	G4double CuLen_H = 2.5;
+    G4VSolid* worldSolid 
+      = new G4Box("worldBox",10.*m,10.*m,10.*m);
+    G4LogicalVolume* worldLogical
+      = new G4LogicalVolume(worldSolid,vac,"worldLogical");
+    G4VPhysicalVolume* worldPhysical
+      = new G4PVPlacement(0,G4ThreeVector(),worldLogical,"worldPhysical",0,
+          false,0,checkOverlaps);
 
-	G4out << "[Cu] (PhiDir, EtaDir, Height) = (" << CuLen_PhiDir << ", " << CuLen_EtaDir << ", " << CuLen_H << ", )" << G4endl;
-
-	G4Box* CuBox 
-	= new G4Box("CuBox",CuLen_PhiDir/2.,CuLen_EtaDir/2.,CuLen_H/2.);
-
-	G4LogicalVolume *CuLogical
-	= new G4LogicalVolume(CuBox, cu, "CuLogical");
-
-	G4VPhysicalVolume* CuPhysical
-	= new G4PVPlacement( 0, G4ThreeVector(), CuLogical, "CuPhysical", worldLogical, false, 0, checkOverlaps );
-
-	return worldPhysical;
+    return worldPhysical;
 }
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void WGR16DetectorConstruction::ConstructSDandField()
 {
 
-}
+}    
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void WGR16DetectorConstruction::ConstructMaterials()
 {
-	G4NistManager* nistManager = G4NistManager::Instance();
-	
-	// -- Vacuum: "Galactic" -- //
-	nistManager->FindOrBuildMaterial("G4_Galactic");
-	nistManager->FindOrBuildMaterial("G4_Cu");
+  G4NistManager* nistManager = G4NistManager::Instance();
+  
+  // Vacuum "Galactic"
+  nistManager->FindOrBuildMaterial("G4_Galactic");
+  nistManager->FindOrBuildMaterial("G4_Cu");
 
-	G4cout << G4endl << "The materials defined are : " << G4endl << G4endl;
-	G4cout << *(G4Material::GetMaterialTable()) << G4endl;
+  G4cout << G4endl << "The materials defined are : " << G4endl << G4endl;
+  G4cout << *(G4Material::GetMaterialTable()) << G4endl;
 }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//
+//
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void WGR16DetectorConstruction::DefineCommands()
 {
-	// Define /WGR16/detector command directory using generic messenger class
-	// fMessenger = new G4GenericMessenger(this, 
-	//                                     "/WGR16/detector/", 
-	//                                     "Detector control");
+    // Define /WGR16/detector command directory using generic messenger class
+/*    fMessenger = new G4GenericMessenger(this, 
+                                        "/WGR16/detector/", 
+                                        "Detector control");
 
-	// G4GenericMessenger::Command& B2ECollAngleCmd
-	//   = fMessenger->DeclareMethodWithUnit("Block2Angle","deg",
-	//       &WGR16DetectorConstruction::SetBlock2Angle,
-	//       "Set rotation angle of exit collimator and corresponding detector");
-	// B2ECollAngleCmd.SetParameterName("angle",true);
-	// B2ECollAngleCmd.SetRange("angle>=-90. && angle<=90");
-	// B2ECollAngleCmd.SetDefaultValue("60.");	
+    G4GenericMessenger::Command& B2ECollAngleCmd
+      = fMessenger->DeclareMethodWithUnit("Block2Angle","deg",
+          &WGR16DetectorConstruction::SetBlock2Angle,
+          "Set rotation angle of exit collimator and corresponding detector");
+    B2ECollAngleCmd.SetParameterName("angle",true);
+    B2ECollAngleCmd.SetRange("angle>=-90. && angle<=90");
+    B2ECollAngleCmd.SetDefaultValue("60.");
+*/
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
