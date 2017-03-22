538592c68f5a70fdd55ff3a7306b02b38934fd02
Kyoengpil Lee <kplee@cern.ch>
2017-03-22 13:34:56 +0900

    investigation: overlap

--- source/src/WGR16DetectorConstruction.cc
+++ source/src/WGR16DetectorConstruction.cc
@@ -445,7 +445,7 @@ G4VPhysicalVolume* WGR16DetectorConstruction::Construct()
 		Trd_rotM.rotateZ(Trd_phi);
 
 		G4ThreeVector Trd_Unit_Z = G4ThreeVector(std::cos(Trd_phi),  std::sin(Trd_phi),0.);
-		G4ThreeVector Trd_position = (radius/std::cos(half_dPhi) + 0.5*CuTrdLen_H)*Trd_Unit_Z; // -- multiply the size of the vector -- //
+		G4ThreeVector Trd_position = (radius/std::cos(half_dPhi) + 0.5*CuTrdLen_H+0.01*m)*Trd_Unit_Z; // -- multiply the size of the vector -- //
 		G4Transform3D Trd_transform = G4Transform3D(Trd_rotM,Trd_position);
 
 		new G4PVPlacement(Trd_transform, CuTrdLogical, "CuTrdPhysical", worldLogical, false, i_cu, checkOverlaps );
